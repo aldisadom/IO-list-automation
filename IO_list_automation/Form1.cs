a41e98ecf2b4f1bb38edd11791edfa2ea9a9aa8c
aldisadom <aldisadom@gmail.com>
2018-07-26 17:00:53 +0300

    efirst experiments

---
+++ IO_list_automation/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            OpenFileDialog file_open = new OpenFileDialog();
+            InitializeComponent();
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("newColumnName", "Column Name in Text");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            dataGridView1.Rows.Add(row);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int x = 1;
+            int y = 0;
+
+            string someString = dataGridView1[x, y].Value.ToString();
+            Console.WriteLine(someString);
+            string pazymetas = dataGridView1.CurrentCell.Value.ToString();
+            Console.WriteLine(pazymetas);
+        }
+
+
+
+
+      
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            design project = new design();
+            project.read_data();
+
+            int count = project.cabinet.Count;
+            for (int i=0; i < count; i++)
+            {
+                richTextBox1.AppendText(project.cabinet[i] + " \n");
+               // richTextBox1.Text = kazkas.listas[i] +" \n";
+            }
+        }
+
+       
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "Inventory_Adjustment_Export.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // Copy DataGridView results to clipboard
+                copyAlltoClipboard();
+
+                object misValue = System.Reflection.Missing.Value;
+                Excel.Application xlexcel = new Excel.Application();
+
+                xlexcel.DisplayAlerts = false; // Without this you will get two confirm overwrite prompts
+                Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                // Format column D as text before pasting results, this was required for my data
+                Excel.Range rng = xlWorkSheet.get_Range("D:D").Cells;
+                rng.NumberFormat = "@";
+
+                // Paste clipboard results to worksheet range
+                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+                // For some reason column A is always blank in the worksheet. ¯\_(ツ)_/¯
+                // Delete blank column A and select cell A1
+                Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
+                delRng.Delete(Type.Missing);
+                xlWorkSheet.get_Range("A1").Select();
+
+                // Save the excel file under the captured location from the SaveFileDialog
+                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlexcel.DisplayAlerts = true;
+                xlWorkBook.Close(true, misValue, misValue);
+                xlexcel.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlexcel);
+
+                // Clear Clipboard and DataGridView selection
+                Clipboard.Clear();
+                dataGridView1.ClearSelection();
+
+                // Open the newly saved excel file
+                if (File.Exists(sfd.FileName))
+                    System.Diagnostics.Process.Start(sfd.FileName);
+            }
+        }
+
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }  
+}
---
+++ IO_list_automation/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    public class design
+    {
+        public  List<string> cabinet = new List<string>();          //collumn 2
+        public  List<string> Module = new List<string>();           //collumn 3
+        public  List<string> Pin = new List<string>();              //collumn 4
+        public  List<string> Chanel = new List<string>();           //collumn 5
+        public  List<string> Function_text = new List<string>();    //collumn 6
+        public  List<string> Page = new List<string>();             //collumn 7
+
+        public void read_data()
+        {
+             OpenFileDialog importfile = new OpenFileDialog();
+            //importfile.Filter = "Excel Documents (*.xls)|*.xls";
+            //importfile.FileName = "Inventory_Adjustment_Export.xls";
+            if (importfile.ShowDialog() == DialogResult.OK)
+            {
+                Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                Excel.Workbook workbook = app.Workbooks.Open(importfile.FileName, true, true);
+                Excel.Worksheet worksheet = workbook.ActiveSheet;
+                int rcount = worksheet.UsedRange.Rows.Count;
+                int ColumnCount = worksheet.UsedRange.Columns.Count;
+                string reiksme = " ";
+                rcount = 10;
+                for (int x = 0; x < ColumnCount; x++)
+                {
+                    for (int i = 0; i < rcount; i++)
+                    {
+                        reiksme = worksheet.Cells[i + 1, 1].Value;
+                        if (reiksme == null)
+                        {
+                            cabinet.Add(" ");
+                        }
+                        else
+                        {
+                            cabinet.Add(reiksme);
+                        }
+                    }
+                }
+                workbook.Close();
+                app.Quit();
+            }
+        }
+    }
+}
